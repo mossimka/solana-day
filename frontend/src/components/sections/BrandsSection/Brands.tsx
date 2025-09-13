be74e98d012831b7dceeeacde96559baea7245c4
mossimka <maksimsarsekeev@gmail.com>
2025-09-13 08:17:57 +0500

    css

--- frontend/src/components/sections/BrandsSection/Brands.tsx
+++ frontend/src/components/sections/BrandsSection/Brands.tsx
@@ -5,7 +5,7 @@ import styles from './Brands.module.css';
 const Brands = () => {
   return (
     <div className='flex flex-col items-center justify-center text-center width-[100%]'>
-      <h2 className='text-2xl font-bold mb-4'>Thanks for supporting us</h2>
+      <h2 className='text-4xl font-bold text-primary mb-4'>Thanks for supporting us</h2>
       <div className="w-full overflow-hidden mt-20 mb-40">
         <div className={styles.brandLogos}>
           <Image src="/kbtu.webp" alt="KBTU" width="150" height="50" />
