4d1fa3669155b0e02d61d998437b364ff3097481
mossimka <maksimsarsekeev@gmail.com>
2025-09-13 07:55:47 +0500

    ci/cd and brands

--- frontend/src/components/sections/BrandsSection/Brands.tsx
+++ frontend/src/components/sections/BrandsSection/Brands.tsx
@@ -6,13 +6,19 @@ const Brands = () => {
   return (
     <div className='flex flex-col items-center justify-center text-center width-[100%]'>
       <h2 className='text-2xl font-bold mb-4'>Thanks for supporting us</h2>
-      <div className={styles.brandLogos}>
-        <Image src="/kbtu.webp" alt="KBTU" width="150" height="50" />
-        <Image src="/decentrathon.webp" alt="Decentrathon" width="150" height="30" />
-        <Image src="/solana.svg" alt="Solana" width="150" height="50" />
-        <Image src="/ministry.webp" alt="Ministry" width="150" height="50" />
-        <Image src="/superteam.webp" alt="Superteam" width="150" height="50" />
-        <Image src="/devs.webp" alt="Devs.kz" width="150" height="50" />
+      <div className="w-full overflow-hidden mt-20 mb-40">
+        <div className={styles.brandLogos}>
+          <Image src="/kbtu.webp" alt="KBTU" width="150" height="50" />
+          <Image src="/decentrathon.webp" alt="Decentrathon" width="300" height="50" />
+          <Image src="/solana.svg" alt="Solana" width="150" height="50" />
+          <Image src="/nfactorial.webp" alt="NFactorial" width="150" height="50" />
+          <Image src="/ministry.webp" alt="Ministry" width="200" height="50" />
+          <Image src="/superteam.webp" alt="Superteam" width="300" height="50" />
+          <Image src="/devs.webp" alt="Devs.kz" width="150" height="50" />
+          <Image src="/kbtu.webp" alt="KBTU" width="150" height="50" />
+          <Image src="/decentrathon.webp" alt="Decentrathon" width="300" height="50" />
+          <Image src="/solana.svg" alt="Solana" width="150" height="50" />
+        </div>
       </div>
     </div>
   )
